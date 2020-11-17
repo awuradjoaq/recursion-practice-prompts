ca8d950754aa92c89a8667fe665b0bf0735762d4
awuradjoaq <awuradjoaq@gmail.com>
2020-11-16 18:01:53 -0800

    finished problem three for recursion

--- src/recursion.js
+++ src/recursion.js
@@ -42,7 +42,16 @@ var sum = function(array) {
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+
+  var sum = 0;
   
+  if (!Array.isArray(array)) {
+    return array;
+  }
+  for (let i = 0;  i < array.length; i++) {
+    sum += arraySum(array[i])
+  }
+  return sum;
 };
 
 // 4. Check if a number is even.
