dd7c45d4296e80f8e248660eff5d2c89cd00c65d
awuradjoaq <awuradjoaq@gmail.com>
2020-11-16 16:31:49 -0800

    finished problem one and two for recursion review

--- src/recursion.js
+++ src/recursion.js
@@ -7,16 +7,42 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  // I - integer
+  // O - integer(product)
+  // C - use recursion
+  // E - 0! = 1
+
+  if (n === 0) {
+    return 1;
+  }
+
+  if (n < 0) {
+    return null;
+  }
+
+  return n * factorial(n - 1);
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  // I - array of integers
+  // O - number
+  // C - use recursion
+  // E - negative numbers, empty array, array with one input
+
+  // base case: if array is empty, return 0
+  if (array.length === 0) {
+    return 0;
+  }
+  // else: return sum of element at beginning of array and rest of elements in array
+  return array[0] + sum(array.slice(1));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  
 };
 
 // 4. Check if a number is even.
