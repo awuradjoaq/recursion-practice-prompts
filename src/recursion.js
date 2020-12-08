9c34eb4dadffa475394ecf23e2f39f7f77d326b9
awuradjoaq <awuradjoaq@gmail.com>
2020-12-08 15:00:57 -0800

    finished number twelve of recursion prompts

--- src/recursion.js
+++ src/recursion.js
@@ -233,11 +233,44 @@ var palindrome = function(string) {
 // modulo(17,5) // 2
 // modulo(22,6) // 4
 var modulo = function(x, y) {
+
+  if (y === 0) {
+    return NaN;
+  }
+
+  if (x < y) {
+    return x;
+  }
+
+  if (x > y && (x < 0 && y < 0)) {
+    return x;
+  }
+
+  if (x >= y && (x > 0 && y > 0)) {
+    return modulo((x - y), y);
+  }
 };
 
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
 var multiply = function(x, y) {
+
+  if (y === 0) {
+    return 0;
+  };
+
+  if (y > 0) {
+    return x + multiply(x , y - 1);
+  }
+
+  if (y < 0 && x < 0) {
+    return -x + multiply(x, y + 1);
+  }
+
+  if (y < 0 && x > 0) {
+    return -x + multiply(x, y + 1);
+  }
+
 };
 
 // 13. Write a function that divides two numbers without using the / operator or
