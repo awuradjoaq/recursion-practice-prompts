3f4c663c9f5f362af8f4bf7b9fb4617fa7d8de2e
awuradjoaq <awuradjoaq@gmail.com>
2021-03-03 19:30:04 -0800

    finished recursion prompts 1-4

--- src/recursion.js
+++ src/recursion.js
@@ -8,65 +8,56 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
-  // I - integer
-  // O - integer(product)
-  // C - use recursion
-  // E - 0! = 1
-
   if (n === 0) {
     return 1;
   }
-
+  if (n > 0) {
+    return n * factorial(n - 1);
+  }
   if (n < 0) {
     return null;
   }
-
-  return n * factorial(n - 1);
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
-  // I - array of integers
-  // O - number
-  // C - use recursion
-  // E - negative numbers, empty array, array with one input
-
-  // base case: if array is empty, return 0
   if (array.length === 0) {
     return 0;
   }
-  // else: return sum of element at beginning of array and rest of elements in array
+
   return array[0] + sum(array.slice(1));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
-
-  var sum = 0;
-
   if (!Array.isArray(array)) {
     return array;
   }
-  for (let i = 0;  i < array.length; i++) {
-    sum += arraySum(array[i])
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    sum += arraySum(array[i]);
   }
   return sum;
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
-  n = Math.abs(n);
-
   if (n === 0) {
     return true;
   }
-  if (n === 1) {
+  if (n === 1 || n === -1) {
     return false
-  };
+  }
+  if (n > 0) {
+    return isEven(n - 2);
+  }
+  if (n < 0) {
+    return isEven(n + 2);
+  }
 
-  return isEven(n - 2);
 };
 
 // 5. Sum all integers below a given integer.
